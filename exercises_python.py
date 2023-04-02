0dbdb4f147220b91c8dc021372025d3fc300fb66
Kaniz Fatima <91112789+kfatima0@users.noreply.github.com>
2023-04-03 03:09:32 +0600

    Add files via upload

---
+++ exercises_python.py
@@ -0,0 +1,14 @@
+##write a program to print the answer to the calculation 6 times and 7
+print (6*7)
+#strings in python
+#In python thers no difference between single or double quotes in python
+print('Give your best shot')
+print("Python is best programming language")
+print('We can even include "quotes" in strings')
+print("Good" + "bye") #joins two words together, to make the single string its called string concatenation
+greeting = "Hi"
+name = input("Please enter your name ")
+print(greeting + ' ' + name)# program got input from user , to that we call input function and then assign 
+##the value it returns to a variable
+
+
