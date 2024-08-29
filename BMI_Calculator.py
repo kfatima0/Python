17ccc26b73aefa7a62b98b393ee60bc4aaf24fea
kfatima0 <kanizfatima662@gmail.com>
2024-08-29 14:46:19 +0600

    Add new feature to BMI_Calculator.py

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,11 +1,23 @@
-height = 1.65
-weight = 84
+#height = 1.65
+#weight = 84
 
-bmi = (84 / 1.64 ** 2)
-print(bmi)
+#bmi = (85 / 1.64 ** 2)
+#print(bmi)
 
-print(int(bmi))
+#print(int(bmi))
 
-print(round(bmi))
+#print(round(bmi))
 
-print(round(bmi, 2))
+#print(round(bmi, 2))
+
+#bmi calculator with interpretations
+height = 1.85
+weight = 85
+bmi = weight /(height ** 2)
+
+if bmi >= 25:
+    print("overweight")
+elif bmi >= 18.5:
+    print("normal weight")
+else:
+    print("underweight")
