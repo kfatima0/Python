20b4e68439a0997cf9121567b15820e9bc6d08f2
kfatima0 <kanizfatima662@gmail.com>
2024-09-01 11:02:52 +0600

    PizzaOrder_DeliveryProgram

---
+++ pizzaorder.py
@@ -0,0 +1,11 @@
+#goal to create python pizza delivery program and the program is going to automatically calculate the bill for the user on a number of options
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+customize_toppings = input("Do you want customizetoppings on our pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+#todo: workout how much they need to pay based on their size choice
+
+#todo: workout how much to add to their bill based on their customize toppings choice.
+
+#todo: work out their final mount based on whether if they want extra cheese.
