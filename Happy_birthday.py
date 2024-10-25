24d0f83c47a3d6b1cda82afc2e88a5214496a1c4
kfatima0 <kanizfatima662@gmail.com>
2024-10-25 23:06:30 +0600

    Greetings

---
+++ Happy_birthday.py
@@ -0,0 +1,7 @@
+def happy_birthday(name):
+    print("Happy Birthday to You!")
+    print("Happy Birthday to You!")
+    print(f"Happy Birthday dear {name},")
+    print("Happy Birthday to You!")
+name = input("Enter the name of the birthday person: ")
+happy_birthday(name)
